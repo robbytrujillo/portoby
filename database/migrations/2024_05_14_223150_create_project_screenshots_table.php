bf17bc8d9838a868e531de3c31e26abf699b5b47
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-15 05:56:59 +0700

    migration db

--- database/migrations/2024_05_14_223150_create_project_screenshots_table.php
+++ database/migrations/2024_05_14_223150_create_project_screenshots_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('project_screenshots', function (Blueprint $table) {
             $table->id();
+            $table->string('screenshot');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
