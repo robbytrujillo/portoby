4ab32b3405685f09a9b2541eb4fcb4363c420f80
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-25 06:37:08 +0700

    Integrate data landing page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class FrontController extends Controller
 {
     public function index() {
-        return view('front.index');
+        // return view('front.index');
+        $projects = Project::orderBy('id', 'desc')->get();
+        return view('front.index', [
+            'projects' => $projects
+        ]);
     }
     
     public function details() {
